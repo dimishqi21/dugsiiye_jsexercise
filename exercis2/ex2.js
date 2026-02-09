3c3a9320378533527a1ba7742a8974204b969e13
dimishqi21 <muhanadibrahim43@gmail.com>
2026-02-09 12:18:19 +0200

    ex2

---
+++ exercis2/ex2.js
@@ -0,0 +1,18 @@
+let x = 15;
+let y = "15";
+// equal
+console.log(x == y); 
+// not equal
+console.log(x != y);
+// strictly equal
+console.log(x === y);
+// strictly not equal
+console.log(x !== y);
+// greater than
+console.log(x > y);
+// greater than or equal to
+console.log(x < y);
+// less than or equal to
+
+console.log(x >= y);
+
